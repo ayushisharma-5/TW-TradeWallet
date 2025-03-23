7a60546f41d518e17a74aaf2bff7c75dbacf4d43
niveditavats1 <139032583+niveditavats1@users.noreply.github.com>
2025-03-23 13:53:43 -0500

    Update About.jsx with Authors Name

--- TradeWallet-Frontend/src/components/about/About.jsx
+++ TradeWallet-Frontend/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import '../../styles/about.css';
 import Author from '../author/Author';
-import userimg from '../../assets/test-image.png';
+import userimg from '../../assets/Tradde-Wallet-Logo.png';
 
 const   About = ({ setUserInParentComponent }) => {
     const handleLogout = (event) => {
@@ -14,9 +14,10 @@ const   About = ({ setUserInParentComponent }) => {
     return (
         <>
             <h1>About us</h1>
-            <Author img={userimg} bio={'This the bio of Author 1'}/>
-            <Author img={userimg} bio={'This the bio of Author 2'}/>
-            <Author img={userimg} bio={'This the bio of Author 3'}/>
+            <Author img={userimg} bio={'This the bio of "Nivedita Vats"'}/>
+            <Author img={userimg} bio={'This the bio of "Sidhansh Chopda"'}/>
+            <Author img={userimg} bio={'This the bio of "Ayushi Sharma"'}/>
+            <Author img={userimg} bio={'This the bio of "Kuldeep Singhji Rathore"'}/>
             <button onClick={handleLogout}>Logout</button>
         </>);
 }
